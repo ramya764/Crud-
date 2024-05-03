4eae33ea7c984ec46d61a7ff0e04bda5c5bc83af
ramya764 <160103251+ramya764@users.noreply.github.com>
2024-05-03 15:46:06 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,100 @@
+.title-box {
+    background-color: pink;
+    padding: 20px;
+}
+
+.manage-box {
+    background-color: yellow;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.add-button {
+    background-color: red;
+    color: #FFFFFF;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    text-decoration: none;
+}
+
+.alert-box {
+    margin-top: 20px;
+}
+
+.table-box {
+    background-color: #000000;
+    padding: 20px;
+}
+
+.table {
+    width: 100%;
+    color: #FFFFFF;
+}
+
+.edit-button {
+    background-color: green;
+    color: #000000;
+    padding: 5px 10px;
+    border: none;
+    border-radius: 3px;
+    text-decoration: none;
+}
+
+.delete-button {
+    background-color: red;
+    color: #FFFFFF;
+    padding: 5px 10px;
+    border: none;
+    border-radius: 3px;
+    text-decoration: none;
+}
+
+.form-box {
+    background-color: #FFFFFF;
+    padding: 20px;
+}
+
+.submit-button {
+    background-color: green;
+    color: #FFFFFF;
+
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    text-decoration: none;
+}
+
+.table {
+    width: 100%;
+    color: #FFFFFF;
+    border-collapse: collapse;
+}
+
+/* CSS for table headers */
+.table th {
+    padding: 8px;
+    text-align: left;
+
+}
+
+/* CSS for table data cells */
+.table td {
+    padding: 8px;
+    text-align: left;
+
+}
+
+
+
+/* Specific styling for ID column */
+.table td:nth-child(1) { /* ID column */
+    text-align: center;
+    width: 50px; /* Adjust width as needed */
+}
+/* Specific styling for ID column */
+.table td:first-child(1) { /* ID column */
+    text-align: left;
+    width: 50px; /* Adjust width as needed */
+}
